4a0e030841f56ddefa231460139e27fe35316cc8
Nitesh Sunku <nsunku99@gmail.com>
2023-07-25 00:33:55 -0700

    set up backend routes and sql connection
    Co-authored-by: Jordan Zolman <PrincePuggo@users.noreply.github.com>
    Co-authored-by: Abe Henderson <a-hend@users.noreply.github.com>

--- src/app/api/search/route.ts
+++ src/app/api/search/route.ts
@@ -1,6 +1,8 @@
 import { NextResponse, NextRequest } from 'next/server'
 import { createEdgeRouter } from "next-connect"
 
+/* https://www.npmjs.com/package/next-connect */
+
 //SETUP FOR NEXT-CONNECT ROUTER
 interface RequestContext {
   params: {
@@ -8,36 +10,38 @@ interface RequestContext {
   };
 };
 
-const router = createEdgeRouter<ResponseContext, RequestContext>();
+const router = createEdgeRouter<NextRequest, RequestContext>();
 
 router
-  .post(async(req, event, next) => {
+  .post(async (req, event, next) => {
     // processing or fetch
     // middleware 1
-    
+
     return next() // for next middleware
   })
 
-  .post(async(req, res, next) => {
+  .post(async (req, res, next) => {
 
     //middleware2
   })
 
-  .post(async(req, res, next) => {
+  .post(async (req, res, next) => {
 
     //middleware2
-    return NextResponse.json({response: response, status: 200})
+    return NextResponse.json({ response: 'response', status: 200 })
   })
 
-
-
+export async function POST(request: NextRequest, ctx: RequestContext) {
+  return router.run(request, ctx);
+}
 
 /// alternative
 
-export async function POST(request: NextRequest) {
 
-  // process data
-  const message = data;
-  return NextResponse.json({});
+// export async function POST(request: NextRequest) {
 
-}
+//   // process data
+//   const message = 'data';
+//   return NextResponse.json({});
+
+// }
